a160b0550e76fb724c3bf3fa7ba4be59d61e58ae
Bishwash <drewwbishwash@gmail.com>
2025-06-07 23:13:06 +0545

    mc

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.get("/", (req, res) => {
 
 app.listen(PORT, async () => {
   console.log(`Server running on port https://localhost:${PORT}`);
-  await connectToDatabase();
+  await connectedtodb();
 });
 
 export default app;
